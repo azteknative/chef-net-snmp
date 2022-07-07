b50f055297b576585242eaf48e5f110a415e9b0c
Peter Green <peter.green@aztek-native.com>
2022-07-07 23:08:12 +0100

    Transition to strings instead of symbols for attribute names.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default[:snmp_v2c][:community_string] = "HS-Bytemark-production"
-default[:snmp_v2c][:mountpoints] = ["/srv", "/var", "/home", "/tmp" ]
+default['snmp_v2c']['community_string'] = "HS-Bytemark-production"
+default['snmp_v2c']['mountpoints'] = ["/srv", "/var", "/home", "/tmp" ]
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-case node[:platform]
+case node['platform']
   when "CentOS","RedHat","Fedora"
   package "net-snmp"  
 end
