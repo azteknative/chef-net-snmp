26392534f1c0f6ba518a073abe5f9624e8959e6c
Peter Green <peter.green@aztek-native.com>
2022-07-07 23:37:42 +0100

    Update metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Dachis Group (UK) Ltd."
-maintainer_email "eu.support+chef@dachisgroup.com"
-license          "Apache 2.0"
-description      "Installs/Configures net-snmp"
+maintainer       'Peter Green'
+maintainer_email 'peter.green@aztek-native.com'
+license          'MIT'
+description      'Installs/Configures net-snmp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          '0.1'
